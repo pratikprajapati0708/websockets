39a5f811dfcc4dd18e05413dd2af400083b21a4c
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-04-21 18:58:13 +0530

    creating websocket server and http

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import WebSocket,{WebSocketServer} from "ws";
+import http from 'http'
+
+//creating an https Server in ws
+
+const server  = http.createServer(function(req,res){
+    console.log((new Date()) + 'Received Request for '+ req.url);
+    res.end('Hi there');
+})
+
+//creating instamce of websocket server
+
+const wss = new WebSocketServer({server});
+
+wss.on('connection',function connection(ws){
+    ws.on('error',console.error);
+
+    ws.on('message',function message(data,isBinary){
+        wss.clients.forEach(function each(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(data,{binary : isBinary});
+            }
+        });
+    });
+    ws.send('Hello ! Message from Server');
+});
